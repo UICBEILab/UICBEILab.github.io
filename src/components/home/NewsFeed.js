d18d2ab3ce5205e3c95290f845ec8aa9e100ecf5
Nghi Bien <nghibien@Nghis-MBP-2.attlocal.net>
2023-07-20 13:19:23 -0500

    update first page

--- src/components/home/NewsFeed.js
+++ src/components/home/NewsFeed.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { Timeline } from 'react-twitter-widgets';
 import Fade from 'react-reveal/Fade';
 import './NewsFeed.css';
-
 const NewsFeed = ({ username }) => {
 
     const twitterUsername = username.replace('@', ''); // Remove the '@' symbol
 
     return (
         <div id="newsfeed">
-            <div className="news-container">
-                <div className="twitter-news">
-                    <div className="smartphone">
-                        <div className="content">
+            <div className="news2-container">
+                <div className="twitter-news2">
+                    <div className="smartphone2">
+                        <div className="content2">
                             <Timeline
                                 dataSource={{
                                     sourceType: 'profile',
--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import { Timeline } from 'react-twitter-widgets';
-import Fade from 'react-reveal/Fade';
-import './News.css';
+// import React from 'react';
+// import { Timeline } from 'react-twitter-widgets';
+// import Fade from 'react-reveal/Fade';
+// import './News.css';
 
-import Awards from './Awards';
+// import Awards from './Awards';
 
-const News = ({ username }) => {
+// const News = ({ username }) => {
 
-    const twitterUsername = username.replace('@', ''); // Remove the '@' symbol
+//     const twitterUsername = username.replace('@', ''); // Remove the '@' symbol
 
-    return (
-        <div id="news">
-            <Fade bottom>
-                <h1>Awards/News</h1>
-            </Fade>
-            <div className="news-container">
-                <div className="awards-container">
-                    <Awards />
-                </div>
+//     return (
+//         <div id="news">
+//             <Fade bottom>
+//                 <h1>Awards/News</h1>
+//             </Fade>
+//             <div className="news-container">
+//                 <div className="awards-container">
+//                     <Awards />
+//                 </div>
 
-                <div className="twitter-news">
-                    <div className="smartphone">
-                        <div className="content">
-                                <Timeline
-                                    dataSource={{
-                                        sourceType: 'profile',
-                                        screenName: twitterUsername,
-                                    }}
-                                    options={{
-                                        tweetLimit: 5,
-                                    }}
-                                />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+//                 <div className="twitter-news">
+//                     <div className="smartphone">
+//                         <div className="content">
+//                                 <Timeline
+//                                     dataSource={{
+//                                         sourceType: 'profile',
+//                                         screenName: twitterUsername,
+//                                     }}
+//                                     options={{
+//                                         tweetLimit: 5,
+//                                     }}
+//                                 />
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
 
-export default News;
+// export default News;
