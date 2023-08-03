520ec237afb4e88fa454d3a92f079a430857fbf4
Nghi Bien <nghibien@Nghis-MBP-2.attlocal.net>
2023-08-03 14:55:55 -0500

    update 2

--- src/components/courses/CoursesData.js
+++ src/components/courses/CoursesData.js
@@ -3,24 +3,24 @@ export const CoursesData = [
         number: "CME 112",
         name: "Evolution of Infrastructure and Society",
         pdf: "/assets/coursesPDF/Evolution-of-Infrastructure_Syllabus.pdf",
-        image: "/assets/coursesPDF/course2.png",
+        image: "/assets/coursesPDF/courseimage3.jpeg",
     },
     {
         number: "CME 494",
         name: "Sustainable Construction",
         pdf: "/assets/coursesPDF/Sustainable-Construction_syllabus.pdf",
-        image: "/assets/coursesPDF/course2.png",
+        image: "/assets/coursesPDF/courseimage3.jpeg",
     },
     {
         number: "CME 494",
         name: "Decision-Making Analysis and Risk Management in Construction",
         pdf: "/assets/coursesPDF/Risk-Management_Syllabus.pdf",
-        image: "/assets/coursesPDF/course2.png",
+        image: "/assets/coursesPDF/courseimage3.jpeg",
     },
     {
         number: "CME 587",
         name: "Construction Estimating and Scheduling",
         pdf: "/assets/coursesPDF/CME_587_Construction-Estimating-and-Scheduling_Syllabus.pdf",
-        image: "/assets/coursesPDF/course2.png",
+        image: "/assets/coursesPDF/courseimage3.jpeg",
     },
 ]
