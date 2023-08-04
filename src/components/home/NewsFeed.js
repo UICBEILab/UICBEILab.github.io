ca0c9d88b993d4d153ae38ea4d12d8785e5578cf
Nghi Bien <nghibien@Nghis-MBP-2.attlocal.net>
2023-08-03 22:40:53 -0500

    up 4

--- src/components/home/NewsFeed.js
+++ src/components/home/NewsFeed.js
@@ -3,35 +3,6 @@ import { Timeline } from 'react-twitter-widgets';
 import Fade from 'react-reveal/Fade';
 import './NewsFeed.css';
 
-// const NewsFeed = ({ username }) => {
-
-//     const twitterUsername = username.replace('@', ''); // Remove the '@' symbol
-
-//     return (
-//         <div id="newsfeed">
-//             <div className="news2-container">
-//                 <div className="twitter-news2">
-//                     <div className="smartphone2">
-//                         <div className="content2">
-//                             <Timeline
-//                                 dataSource={{
-//                                     sourceType: 'profile',
-//                                     screenName: twitterUsername,
-//                                 }}
-//                                 options={{
-//                                     tweetLimit: 5,
-//                                 }}
-//                             />
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default NewsFeed;
-
 const NewsFeed = ({ username }) => {
     const twitterUsername = username.replace('@', ''); // Remove the '@' symbol
 
