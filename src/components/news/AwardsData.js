f35ed334ca73a977387f1e25ef9cfc934da00af2
UICBEILab <140451246+UICBEILab@users.noreply.github.com>
2023-08-10 16:23:43 -0500

    AwardsData.js

--- src/components/news/AwardsData.js
+++ src/components/news/AwardsData.js
@@ -1,6 +1,6 @@
 export const AwardsData = [
    {
-        title: 'Grant Awarded by Dpeartment of Energy and Oak Ridge National Lab',
+        title: 'Grant Awarded by Department of Energy and Oak Ridge National Lab',
         content: "We are pleased to announce that our team (PI: Dr. Karatas) was awarded $29,950 grant by DOE for the project titled \"Analyzing the Impact of Windows Air Leakage on Energy Consumption of Low-Income Residential Houses\". This project aims to investigate the impact of air leakage on on the energy efficiency of buildings. Air leakage through window cracks and joints are majorly responsible for the building’s heating and cooling energy loss. According to the ORNL 2020 study report, low-income households carry a burden of spending 13.9% of total income on energy bills versus 3.0% for other households, which is an overwhelming share of expense for a developing community. Energy efficiency measures in residential housing can be captured with deep energy retrofits, which increase affordability for residents while in the long-term helping preserve affordable housing by lowering operating expenses. However, it is still unknown how much air infiltration and leakage occurs through window.",
     },
     {
