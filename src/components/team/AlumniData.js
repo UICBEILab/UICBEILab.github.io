aa8d9d41192a62718b3f981eea86fc86891b8b55
UICBEILab <140451246+UICBEILab@users.noreply.github.com>
2023-08-28 13:37:56 -0500

    AlumniData.js

--- src/components/team/AlumniData.js
+++ src/components/team/AlumniData.js
@@ -1,16 +1,13 @@
 export const AlumniData = [
     {
         aKey: "1",
-        title: "CURA Undergraduate Student",
+        title: "CURA Undergraduate Awardee",
         name: "Jildardo Luna",
         image: "/assets/TeamPic/JildardoLuna.jpeg",
         resumeLink: "",
         linkedinLink: "https://www.linkedin.com/in/jildardo-luna-96744214b/",
         content: [
-            "Jildardo works as a CURA awarded at BEI Lab.",
-            "He is pursuing his bachelor’s degree in Civil & Material Engineering at UIC.",
-            "He has a passion for environmental engineering and sustainability.",
-            "In his free time, he attempts to set up smart devices at home that work ten percent of the time, embroiders cute animals into old clothing, takes care of his snail (Blenny) and his betta (Baigo), and eats tomatoes from his tomato plant.",
+            "",
         ]
     },
 ];
