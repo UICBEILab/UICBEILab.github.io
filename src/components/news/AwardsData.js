5cfa35d9ca7bd774781863cd0610dd167d7f43dd
Nghi Bien <nghibien@Nghis-MBP-2.attlocal.net>
2023-10-15 22:52:00 -0500

    update publications

--- src/components/news/AwardsData.js
+++ src/components/news/AwardsData.js
@@ -5,7 +5,7 @@ export const AwardsData = [
     },
     {
         title: 'Grant Awarded by STO Corporation',
-        content: "We are pleased to announce that our team (PI: Dr. Karatas) was awarded $212,000 grant by STO Corporation for the project titled \"Comparing Actual and Theoretical Energy Efficiency Performances of Prefab Building Assemblies\". This project aims to measure and evaluate the actual energy efficiency of prefab building assemblies. This project is expected to bridge the gap between actual energy consumption vs. predicted energy consumption of buildings and will help decision-makers improve their building design and energy performance analysis, and reduce their operational costs. Preparations for this project are already underway!",
+        content: "We are pleased to announce that our team (PI: Dr. Karatas) was awarded $212,000 grant by STO Corporation for the project titled \"Comparing Actual and Theoretical Energy Efficiency Performances of Prefab Building Assemblies\". This project aims to measure and evaluate the actual energy efficiency of prefab building assemblies. This project is expected to bridge the gap between actual energy consumption vs. predicted energy consumption of buildings and will help decision-makers improve their building design and energy performance analysis, and reduce their operational costs. Project Start March 2022- Finish December 2023 (Expected)",
     },
     {
         title: 'Grant Awarded by STO Corporation',
