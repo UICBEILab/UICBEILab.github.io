b9521ece41a0b149e34a511966e7365d84bba9d8
Nghi Bien <nghibien@Nghis-MBP-2.attlocal.net>
2023-10-15 22:03:24 -0500

    update 2 grant

--- src/components/news/AwardsData.js
+++ src/components/news/AwardsData.js
@@ -7,4 +7,12 @@ export const AwardsData = [
         title: 'Grant Awarded by STO Corporation',
         content: "We are pleased to announce that our team (PI: Dr. Karatas) was awarded $212,000 grant by STO Corporation for the project titled \"Comparing Actual and Theoretical Energy Efficiency Performances of Prefab Building Assemblies\". This project aims to measure and evaluate the actual energy efficiency of prefab building assemblies. This project is expected to bridge the gap between actual energy consumption vs. predicted energy consumption of buildings and will help decision-makers improve their building design and energy performance analysis, and reduce their operational costs. Preparations for this project are already underway!",
     },
+    {
+        title: 'Grant Awarded by STO Corporation',
+        content: "We are excited to introduce our groundbreaking project sponsored by STO Corp. BEI Lab received $199,950 to conduct research studies on comparing the thermal performance and water/air resistance of wall panel joints in both prefab and precast building assemblies. As the industry currently leans towards precast panels, largely due to an experiential knowledge void about prefab performance, our research aims to provide a comprehensive evaluation of both, paving the way for informed choices in modern construction. This endeavor not only fills a critical research void but also equips construction professionals with insights essential for future projects.",
+    },
+    {
+        title: 'UIC Internal Grant Awarded for Arctic Architectural Research',
+        content: "We're elated to share that we have been honored with a $15,000 internal grant from UIC to delve into the architectural challenges presented by the Arctic region. This generous support enables us to further explore and address the unique design considerations and constraints posed by this extreme environment. With this initiative, we're eager to contribute valuable insights to the broader architectural discourse and inspire sustainable solutions for Arctic construction. Join us as we embark on this icy, yet thrilling, academic adventure!",
+    },
 ]
